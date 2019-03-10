fa727b0486d986c0d8305cf3c3c6e6f35c01ac77
creatorfromhell <daniel.viddy@gmail.com>
2019-03-09 22:12:41 -0500

    Balance & Account Logic

--- src/net/tnemc/hellconomy/core/common/account/Balance.java
+++ src/net/tnemc/hellconomy/core/common/account/Balance.java
@@ -1,5 +1,13 @@
 package net.tnemc.hellconomy.core.common.account;
 
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.CompositePK;
+import org.javalite.activejdbc.annotations.DbName;
+import org.javalite.activejdbc.annotations.Table;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
 /**
  * Created by creatorfromhell.
  *
@@ -9,5 +17,60 @@ package net.tnemc.hellconomy.core.common.account;
  * International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
  * or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
  */
-public class Balance {
+
+@DbName("HellConomy")
+@Table("hellco_balances")
+@CompositePK({"balance_owner", "balance_server", "balance_world", "balance_currency"})
+public class Balance extends Model {
+
+  //balance_owner
+  //balance_server
+  //balance_world
+  //balance_currency
+  //balance_amount
+
+  public static boolean add(UUID owner, String server, String world, String currency, BigDecimal amount) {
+    boolean exists = exists(owner, server, world, currency);
+    Balance balance = (exists)? getBalance(owner, server, world, currency) : new Balance();
+
+    balance.set("balance_owner", owner.toString());
+    balance.set("balance_server", server);
+    balance.set("balance_world", world);
+    balance.set("balance_currency", currency);
+    balance.set("balance_amount", amount);
+
+    return balance.saveIt();
+  }
+
+  public static BigDecimal addBalanceValue(UUID owner, String server, String world, String currency, BigDecimal amount) {
+    final BigDecimal result = (exists(owner, server, world, currency))? getBalanceValue(owner, server, world, currency).add(amount) : amount;
+
+    add(owner, server, world, currency, result);
+
+    return result;
+  }
+
+  public static BigDecimal subBalanceValue(UUID owner, String server, String world, String currency, BigDecimal amount) {
+    final BigDecimal result = (exists(owner, server, world, currency))? getBalanceValue(owner, server, world, currency).subtract(amount) : BigDecimal.ZERO;
+
+    add(owner, server, world, currency, result);
+
+    return result;
+  }
+
+  public static BigDecimal getBalanceValue(UUID owner, String server, String world, String currency) {
+    return getBalance(owner, server, world, currency).getBigDecimal("balance_amount");
+  }
+
+  public static Balance getBalance(UUID owner, String server, String world, String currency) {
+    return Balance.findFirst("balance_owner = ? AND balance_server = ? AND balance_world = ? AND balance_currency = ?", owner.toString(), server, world, currency);
+  }
+
+  public static boolean exists(UUID owner, String server, String world, String currency) {
+    return Balance.findFirst("balance_owner = ? AND balance_server = ? AND balance_world = ? AND balance_currency = ?", owner.toString(), server, world, currency) != null;
+  }
+
+  public static void delete(UUID owner, String server, String world, String currency) {
+    Balance.delete("balance_owner = ? AND balance_server = ? AND balance_world = ? AND balance_currency = ?", owner.toString(), server, world, currency);
+  }
 }
--- src/net/tnemc/hellconomy/core/common/account/HellAccount.java
+++ src/net/tnemc/hellconomy/core/common/account/HellAccount.java
@@ -1,5 +1,12 @@
 package net.tnemc.hellconomy.core.common.account;
 
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.CompositePK;
+import org.javalite.activejdbc.annotations.DbName;
+import org.javalite.activejdbc.annotations.Table;
+
+import java.util.UUID;
+
 /**
  * Created by creatorfromhell.
  *
@@ -10,6 +17,48 @@ package net.tnemc.hellconomy.core.common.account;
  * or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
  */
 
+@DbName("HellConomy")
+@Table("hellco_accounts")
+@CompositePK({"account_id"})
+public class HellAccount extends Model {
 
-public class HellAccount {
+  //account_id
+  //account_display
+  //account_created
+  //account_player
+
+  public static boolean add(UUID id, String display, long created, boolean player) {
+    boolean exists = exists(id);
+    HellAccount account = (exists)? getAccount(id) : new HellAccount();
+
+    if(!exists) account.set("account_id", freeID().toString());
+    account.set("account_display", display);
+    if(!exists) account.set("account_created", created);
+    if(!exists) account.set("account_player", player);
+
+    return account.saveIt();
+  }
+
+  public static HellAccount getAccount(UUID id) {
+    return HellAccount.findFirst("account_id = ?", id.toString());
+  }
+
+  public static UUID freeID() {
+    UUID id = UUID.randomUUID();
+    if(exists(id)) {
+      //should never happen.
+      while(exists(id)) {
+        id = UUID.randomUUID();
+      }
+    }
+    return id;
+  }
+
+  public static boolean exists(UUID id) {
+    return HellAccount.findFirst("account_id = ?", id.toString()) != null;
+  }
+
+  public static void delete(final UUID id) {
+    HellAccount.delete("village_id = ?", id.toString());
+  }
 }
